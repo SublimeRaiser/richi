4c77cb105303db86a851ef3117e2584a8b0fc2ea
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2020-04-06 12:14:41 +0700

    cut relation to person entity for all the obligation entities

--- src/Entity/Operation/BaseOperationObligation.php
+++ src/Entity/Operation/BaseOperationObligation.php
@@ -2,7 +2,6 @@
 
 namespace App\Entity\Operation;
 
-use App\Entity\Person;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -10,31 +9,4 @@ use Doctrine\ORM\Mapping as ORM;
  */
 abstract class BaseOperationObligation extends BaseOperation
 {
-    /**
-     * @var Person|null
-     *
-     * @ORM\ManyToOne(targetEntity="App\Entity\Person")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    protected $person;
-
-    /**
-     * @return Person|null
-     */
-    public function getPerson(): ?Person
-    {
-        return $this->person;
-    }
-
-    /**
-     * @param Person|null $person
-     *
-     * @return self
-     */
-    public function setPerson(?Person $person): self
-    {
-        $this->person = $person;
-
-        return $this;
-    }
 }
