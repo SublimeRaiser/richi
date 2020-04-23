9c7fd236aebc8db522cd6021058c46b8b7618c21
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2020-04-23 13:19:54 +0700

    fix voter for the debt entity

--- src/Security/Voter/DebtVoter.php
+++ src/Security/Voter/DebtVoter.php
@@ -2,7 +2,7 @@
 
 namespace App\Security\Voter;
 
-use App\Entity\Debt;
+use App\Entity\Obligation\Debt;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
